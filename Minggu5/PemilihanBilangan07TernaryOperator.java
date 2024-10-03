d2dbd0e9bf035f111450684f05ef228cf3d8e3f6
bavucadoz <aryabayusamodra@gmail.com>
2024-10-03 07:51:02 +0700

    menambahkan tugas

--- Minggu5/PemilihanBilangan07TernaryOperator.java
+++ Minggu5/PemilihanBilangan07TernaryOperator.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 public class PemilihanBilangan07TernaryOperator {
-    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan sebuah angka: ");
