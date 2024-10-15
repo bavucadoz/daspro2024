2629c7c5e23b13dff6aeec875d0b52b9313213e2
bavucadoz <aryabayusamodra@gmail.com>
2024-10-15 18:44:38 +0700

    Menambahkan kode program untuk menentukan Mahasiswa yang lulus dan tidak lulus

--- Minggu7/SiakadFor07.java
+++ Minggu7/SiakadFor07.java
@@ -5,6 +5,7 @@ public class SiakadFor07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
 
         // membuat struktur perulangan
         for (int i = 1; i <= 10; i++) {
@@ -15,11 +16,19 @@ public class SiakadFor07 {
             }
             if (nilai < terendah) {
                 terendah = nilai;
+            }                           
+        //Menghitung mahasiswa yang lulus & tidak lulus
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
             }
         }
 
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa yang lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + tidakLulus);
 
         sc.close();
     }
