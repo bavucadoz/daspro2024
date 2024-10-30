6092c8256b411f4062941fb9b2ada3026b7e2621
bavucadoz <aryabayusamodra@gmail.com>
2024-10-30 15:52:38 +0700

    Modifikasi program untuk menerima jumlah elemen berdasarkan input dari pengguna serta menampilkan rata-rata nilai lulus dan rata-rata nilai tidak lulus

--- Minggu10/ArrayRataNilai07.java
+++ Minggu10/ArrayRataNilai07.java
@@ -3,25 +3,31 @@ import java.util.Scanner;
 public class ArrayRataNilai07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
-        int jumlahLulus = 0;
+
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int jumlahMhs = sc.nextInt();
+        int[] nilaiMhs = new int[jumlahMhs];
+        double totalLulus = 0, totalTidakLulus = 0;
+        double rata2Lulus, rata2TidakLulus;
+        int jumlahLulus = 0, jumlahTidakLulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i]= sc.nextInt();
             if (nilaiMhs[i] > 70) {
                 jumlahLulus++;
+                totalLulus += nilaiMhs[i];
+            } else {
+                jumlahTidakLulus++;
+                totalTidakLulus += nilaiMhs[i];
             }
         }
 
-        for (int i = 0; i < nilaiMhs.length; i++ ) {
-            total += nilaiMhs[i];
-        }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = "+rata2);
-        System.out.println("Jumlah mahasiswa yang lulus = "+jumlahLulus);
+        rata2Lulus = totalLulus/jumlahLulus;
+        rata2TidakLulus = totalTidakLulus/jumlahTidakLulus;
+
+        System.out.println("Rata-rata nilai lulus = "+rata2Lulus);
+        System.out.println("Rata-rata nilai tidak lulus = "+rata2TidakLulus);
 
         sc.close();
     }
