d5ee4928f2af7790ee4aaf8d27599e248f5311c2
bavucadoz <aryabayusamodra@gmail.com>
2024-10-30 13:27:05 +0700

    Modifikasi program dalam Percobaan 1 sesuai Pertanyaan

--- Minggu10/ArrayBilangan07.java
+++ Minggu10/ArrayBilangan07.java
@@ -3,14 +3,12 @@ package Minggu10;
 public class ArrayBilangan07 {
     public static void main(String[] args) {
         int[] bil = new int[4];
-        bil[0]=5;
-        bil[1]=13;
-        bil[2]=-7;
-        bil[3]=17;
+        bil[0]=(int) 5.0;
+        bil[1]=12867;
+        bil[2]=(int) 7.5;
+        bil[3]=2000000;
         
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        for (int i = 0; i <= 4; i++)
+            System.out.println(bil[i]);
     }
 }
