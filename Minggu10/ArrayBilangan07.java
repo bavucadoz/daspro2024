623745b98c01fa24b865624070f4e30a9e0e306b
bavucadoz <aryabayusamodra@gmail.com>
2024-10-30 13:32:57 +0700

    Memperbaiki Program dalam Percobaan 1

--- Minggu10/ArrayBilangan07.java
+++ Minggu10/ArrayBilangan07.java
@@ -3,12 +3,14 @@ package Minggu10;
 public class ArrayBilangan07 {
     public static void main(String[] args) {
         int[] bil = new int[4];
+        
         bil[0]=(int) 5.0;
         bil[1]=12867;
         bil[2]=(int) 7.5;
         bil[3]=2000000;
         
-        for (int i = 0; i <= 4; i++)
+        for (int i = 0; i <= 4; i++) {
             System.out.println(bil[i]);
+        }
     }
 }
