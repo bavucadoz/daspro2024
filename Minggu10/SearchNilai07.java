64fed156d5d060c38695e0788abab50593c5ebf3
bavucadoz <aryabayusamodra@gmail.com>
2024-11-05 21:07:20 +0700

    Memperbaiki program

--- Minggu10/SearchNilai07.java
+++ Minggu10/SearchNilai07.java
@@ -6,7 +6,6 @@ public class SearchNilai07 {
         System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
         int banyakNilai = sc.nextInt();
         int[] arrNilai = new int[banyakNilai];
-        int hasil = 0;
 
         for (int i = 0; i < arrNilai.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
@@ -15,21 +14,21 @@ public class SearchNilai07 {
 
         System.out.print("Masukkan nilai yang ingin dicari: ");
         int key = sc.nextInt();
+        int hasil = -1;
 
         for (int i = 0; i < arrNilai.length; i++) {
             if (key == arrNilai[i]) {
                 hasil = i+1;
-                System.out.println();
-                System.out.println("Nilai "+key+" ketemu, merupakan nilai mahasiswa ke-"+hasil);
-                System.out.println();
                 break;
-            } else {
-                System.out.println();
-                System.out.println("Nilai yang dicari tidak ditemukan");
-                System.out.println();
-                break;
-            }
+            } 
         }
+        System.out.println();   
+            if (hasil != -1) {
+                System.out.println("Nilai " + key + " ketemu, merupakan nilai Mahasiswa ke-" + (hasil+1));
+            } else {
+                System.out.println("Nilai yang dicari tidak ditemukan");
+            }
+        System.out.println();
 
         
         sc.close();
