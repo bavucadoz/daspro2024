916930d08c1fae7338fb986666cc7606fcf65d3b
bavucadoz <aryabayusamodra@gmail.com>
2024-11-05 21:29:18 +0700

    Tugas No.1 : Membuat Program Menyimpan dan Mengelola Nilai Mahasiswa

--- Minggu10/NilaiMahasiswaArray.java
+++ Minggu10/NilaiMahasiswaArray.java
@@ -3,21 +3,40 @@ import java.util.Scanner;
 public class NilaiMahasiswaArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Masukkan jumlah mahasiswa : ");
-        int n = sc.nextInt();
+        int jmlMHS, totalNilai = 0;
+        double rata2;
 
-        int nilai[]= new int[n];
-        int sum=0;
-        for (int i<n;)
+        System.out.print("Masukkan Jumlah Mahasiswa = ");
+        jmlMHS = sc.nextInt();
+        int[] NilaiMHS = new int[jmlMHS];
 
-        int min, max;
-        min = max = nilai[0];
-        for (int i=1;i<n;i++) {
-            if (nilai(i)=max) {
-                max=nilai[i];
-            } if (nilai(i)=min) {
-                min=nilai[i];
+        for (int i = 0; i < jmlMHS; i++) {
+            System.out.print("Masukkan nilai Mahasiswa ke-" + (i + 1) + ": ");
+            NilaiMHS[i] = sc.nextInt();
+        }
+
+        int nilaiTertinggi = NilaiMHS[0];
+        int nilaiTerendah = NilaiMHS[0];
+
+        for (int i = 0; i < jmlMHS; i++) {
+            totalNilai += NilaiMHS[i];  
+            if (NilaiMHS[i] > nilaiTertinggi) {
+                nilaiTertinggi = NilaiMHS[i];
+            }
+            if (NilaiMHS[i] < nilaiTerendah) {
+                nilaiTerendah = NilaiMHS[i];
             }
         }
+        rata2 = (double) totalNilai / jmlMHS;
+        System.out.println();
+        System.out.println("Nilai rata-rata mahasiswa adalah: " + rata2);
+        System.out.println("Nilai tertinggi mahasiswa adalah: " + nilaiTertinggi);
+        System.out.println("Nilai terendah mahasiswa adalah: " + nilaiTerendah);
+        System.out.println("Semua nilai mahasiswa:");
+        System.out.println();
+        for (int i = 0; i < jmlMHS; i++) {
+            System.out.println("Nilai mahasiswa ke-" + (i + 1) + ": " + NilaiMHS[i]);
+        }
+        sc.close();
     }
 }
