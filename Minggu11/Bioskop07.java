e98f65ab247eaf8b74a931961ebe7d834464c7df
bavucadoz <aryabayusamodra@gmail.com>
2024-11-12 12:52:50 +0700

    Hasil Pertanyaan dari Percobaan 1

--- Minggu11/Bioskop07.java
+++ Minggu11/Bioskop07.java
@@ -13,12 +13,26 @@ public class Bioskop07 {
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
+        penonton[3][1] = "Hana";
 
-        //menampilkan isi elemen array
-        System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);     
-        System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
-        System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
-        System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+        //Menambahkan kode program
+        System.out.println(penonton.length);
+        
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: " + barisPenonton.length);
+        }
 
+        //menampilkan nama penonton untuk setiap baris
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton setiap baris ke-" + (i+1) + ": " + String.join(",", penonton[i]));
+        }
+        
+        //kode program menampilkan nama penonton baris ke-3
+        System.out.println("Penonton pada baris ke-3: ");
+
+        for (String i : penonton[2]) {
+            System.out.println(i);
+        }
+        
     }
 }
