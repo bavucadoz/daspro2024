d7c55008266bd4aff637063edad0efa6fcbb8261
bavucadoz <aryabayusamodra@gmail.com>
2024-11-29 19:35:47 +0700

    Modifikasi kode program dengan menambahkan Parameter kodePromo

--- Minggu14/Kafe07.java
+++ Minggu14/Kafe07.java
@@ -2,13 +2,21 @@ package Minggu14;
 
 public class Kafe07 {
     public static void main (String[]args) {
-        Menu("Andi", true);
+        Menu("Andi", true, "DISKON50");
     }
     
-public static void Menu(String namaPelanggan, boolean isMember) {
+public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
 
     if (isMember) {
         System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+    }
+
+    if (kodePromo.equals("DISKON50")) {
+        System.out.println("Selamat! "+namaPelanggan+ " mendapatkan diskon 50% dengan kode promo " + kodePromo);
+    } else if (kodePromo.equals("DISKON30")) {
+        System.out.println("Selamat! " +namaPelanggan+ " mendapatkan diskon 30% dengan kode promo " + kodePromo);
+    } else {
+        System.out.println("Kode promo tidak valid: " + kodePromo);
     }
     
     System.out.println("===== MENU RESTO KAFE =====");
