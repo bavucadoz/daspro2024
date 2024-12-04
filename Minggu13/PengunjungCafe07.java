026e3e0a78cdf7761b67bcda02c30592c73d04c1
bavucadoz <aryabayusamodra@gmail.com>
2024-12-04 13:43:38 +0700

    Membuat Menu menampilkan pengunjung cafe dengan String...

--- Minggu13/PengunjungCafe07.java
+++ Minggu13/PengunjungCafe07.java
@@ -6,9 +6,11 @@ public class PengunjungCafe07 {
     }
 
     public static void daftarPengunjung(String... namaPengunjung) {
-        System.out.println("Daftar nama pengunjung:");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
+        System.out.println("Daftar nama pengunjung : ");
+        /*for (int i = 0; i < namaPengunjung.length; i++) {
+            System.out.println("- "+namaPengunjung[i]);*/
+        for (String namaPeserta : namaPengunjung) {
+            System.out.println("- " + namaPeserta);
         }
     }
 }
